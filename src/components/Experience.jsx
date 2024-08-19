6132a325f5455f1c365187af4737fa3b274cc47f
ErShivnandan <ec1912@global.org.in>
2024-08-20 02:25:47 +0530

    Code added with build

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,19 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, useTexture } from "@react-three/drei";
+import { Avatar } from "./Avatar";
+import { useThree } from "@react-three/fiber";
 
 export const Experience = () => {
+  const texture = useTexture( "/Textures/texture.png");
+  const viewport = useThree((state)=> state.viewport);
+
   return (
     <>
-      <OrbitControls />
+      {/* <OrbitControls /> */}
+      <Avatar position={[0, -3, 5]} scale={2}/>
+      <Environment preset="sunset"/>
       <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
+        <planeGeometry args={[viewport.width, viewport.height]}/>
+        <meshBasicMaterial map={texture} />
       </mesh>
     </>
   );
